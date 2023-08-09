911bbb62624b88ad561e28910241027d06e24456
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2023-08-09 09:14:55 -0300

    update
    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 interface ShelfProps {
   x: number;
   z: number;
   occupied: boolean;
   onClick: () => void;
+  position: [number, number, number];
 }
 
-const shelfSize = 0.8; // Tamanho da prateleira
-const spacing = 0.2; // Espaçamento entre as prateleiras e colunas
+const shelfSize = 0.8;
+const spacing = 0.2;
 
-const Shelf3D: React.FC<ShelfProps> = ({ x, z, occupied, onClick }) => {
+const handleCreateWarehouse = (rows: number, columns: number) => {
+  const newWarehouse: boolean[][] = Array.from({ length: rows }, () =>
+    Array.from({ length: columns }, () => false)
+  );
+  return newWarehouse;
+};
+
+const getShelfPosition = (x: number, z: number, rows: number, columns: number): [number, number, number] => {
+  const offsetX = (columns - 1) * (shelfSize + spacing) * 0.5;
+  const offsetZ = (rows - 1) * (shelfSize + spacing) * 0.5;
+
+  return [
+    (x - columns / 2) * (shelfSize + spacing) + offsetX,
+    0,
+    (z - rows / 2) * (shelfSize + spacing) + offsetZ,
+  ];
+};
+
+const Shelf3D: React.FC<ShelfProps> = ({ x, z, occupied, onClick, position }) => {
   return (
-    <mesh onClick={onClick} position={[x * (shelfSize + spacing), 0, z * (shelfSize + spacing)]}>
+    <mesh onClick={onClick} position={position}>
       <boxGeometry args={[shelfSize, 0.1, shelfSize]} />
       <meshStandardMaterial color={occupied ? '#ff7675' : '#42cd62'} />
+    </mesh>
+  );
+};
+
+const AvailablePositionHighlight: React.FC<{ position: [number, number, number] }> = ({ position }) => {
+  const ref = useRef<THREE.Mesh>(null);
+  const [highlighted, setHighlighted] = useState(false);
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.01;
+      if (highlighted) {
+        ref.current.scale.x = Math.sin(Date.now() * 0.005) * 0.1 + 1;
+        ref.current.scale.z = Math.sin(Date.now() * 0.005) * 0.1 + 1;
+      }
+    }
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHighlighted(!highlighted);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [highlighted]);
+
+  return (
+    <mesh ref={ref} position={position}>
+      <boxGeometry args={[shelfSize, 0.1, shelfSize]} />
+      <meshStandardMaterial color={'#5040e0'} />
     </mesh>
   );
 };
@@ -26,11 +76,10 @@ const App: React.FC = () => {
   const [rows, setRows] = useState<number>(0);
   const [columns, setColumns] = useState<number>(0);
   const [warehouse, setWarehouse] = useState<boolean[][]>([]);
+  const [availablePosition, setAvailablePosition] = useState<[number, number, number] | null>(null);
 
-  const handleCreateWarehouse = () => {
-    const newWarehouse: boolean[][] = Array.from({ length: rows }, () =>
-      Array.from({ length: columns }, () => false)
-    );
+  const handleCreateWarehouseAndSetState = () => {
+    const newWarehouse = handleCreateWarehouse(rows, columns);
     setWarehouse(newWarehouse);
   };
 
@@ -38,6 +87,11 @@ const App: React.FC = () => {
     const updatedWarehouse = [...warehouse];
     updatedWarehouse[rowIndex][columnIndex] = !updatedWarehouse[rowIndex][columnIndex];
     setWarehouse(updatedWarehouse);
+
+    if (!updatedWarehouse[rowIndex][columnIndex]) {
+      const position = getShelfPosition(columnIndex, rowIndex, rows, columns);
+      setAvailablePosition(position);
+    }
   };
 
   const occupiedCount = warehouse.flat().filter((occupied) => occupied).length;
@@ -48,7 +102,7 @@ const App: React.FC = () => {
       <div className="sidebar">
         <div className="inputs">
           <div className="input-group">
-            <label>Rows:</label>
+            <label>Linhas:</label>
             <input
               className="styled-input"
               type="number"
@@ -57,7 +111,7 @@ const App: React.FC = () => {
             />
           </div>
           <div className="input-group">
-            <label>Columns:</label>
+            <label>Colunas:</label>
             <input
               className="styled-input"
               type="number"
@@ -65,13 +119,13 @@ const App: React.FC = () => {
               onChange={(e) => setColumns(parseInt(e.target.value))}
             />
           </div>
-          <button className="styled-button" onClick={handleCreateWarehouse}>
-            Create Warehouse
+          <button className="styled-button" onClick={handleCreateWarehouseAndSetState}>
+            Criar armazém
           </button>
         </div>
         <div className="counts">
-          <p className="occupied">Occupied: {occupiedCount}</p>
-          <p className="available">Available: {availableCount}</p>
+          <p className="occupied">Ocupado: {occupiedCount}</p>
+          <p className="available">Disponível: {availableCount}</p>
         </div>
       </div>
       <div className="canvas-container">
@@ -80,7 +134,7 @@ const App: React.FC = () => {
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} />
             <OrbitControls />
-            <group position={[-columns / 2, 0, -rows / 2]}>
+            <group position={[0, 0, 0]}>
               {warehouse.map((row, rowIndex) =>
                 row.map((occupied, columnIndex) => (
                   <Shelf3D
@@ -89,9 +143,31 @@ const App: React.FC = () => {
                     z={rowIndex}
                     occupied={occupied}
                     onClick={() => handleToggleShelf(rowIndex, columnIndex)}
+                    position={getShelfPosition(columnIndex, rowIndex, rows, columns)}
                   />
                 ))
               )}
+              {availablePosition && (
+                <group>
+                  <AvailablePositionHighlight position={availablePosition} />
+                </group>
+              )}
+              {/* Adiciona a borda preta */}
+              <mesh
+                position={[
+                  (columns - 1) * (shelfSize + spacing) * 0.3,
+                  -0.05,
+                  (rows - 1) * (shelfSize + spacing) * 0.3,
+                ]}
+                scale={[
+                  columns * (shelfSize + spacing) + spacing,
+                  0.1,
+                  rows * (shelfSize + spacing) + spacing,
+                ]}
+              >
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color="black" transparent opacity={0.5} />
+              </mesh>
             </group>
           </Canvas>
         )}
